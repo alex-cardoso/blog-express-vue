efff7a46139fea2458ceabc60434db84e728c759
Alexandre Cardoso <xandecar@hotmail.com>
2020-03-31 10:46:03 -0300

    Webpack agora minificando o css em produção(build)

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-        public: './src/main.js',
-        dashboard: './src/dashboard.js',
-        admin: './src/admin.js',
+        public: ['./src/main.js', './src/assets/styles_main.css'],
+        dashboard: ['./src/dashboard.js', './src/assets/styles_dashboard.css'],
+        admin: ['./src/admin.js', './src/assets/styles_admin.css'],
     },
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: './[name].bundle.js',
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['vue-style-loader', 'css-loader'],
+                use: [
+                    'vue-style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
             },
             {
                 test: /\.vue$/,
                 loader: 'vue-loader',
-                options: {
-                    loaders: {},
-                },
             },
             {
                 test: /\.js$/,
@@ -32,8 +38,12 @@ module.exports = {
     },
     plugins: [
         new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /pt-br/),
     ],
+    // paths simbolicos
     resolve: {
         alias: {
             vue$: 'vue/dist/vue.esm.js',
@@ -41,15 +51,14 @@ module.exports = {
         extensions: ['*', '.js', '.vue', '.json'],
     },
     optimization: {
-        minimize: process.env.NODE_ENV === 'development' ? false : true,
+        // minimize: process.env.NODE_ENV === 'development' ? false : true,
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
     },
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true,
-        overlay: true,
-    },
-    performance: {
-        hints: false,
-    },
-    devtool: '#eval-source-map',
 };
